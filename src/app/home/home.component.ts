7574a5ee1d4622574b8108e2fd3701b5305738d4
Sheldon Debra <54963942+sheldondebra@users.noreply.github.com>
2024-04-23 21:24:45 +0000

    new update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocationComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
