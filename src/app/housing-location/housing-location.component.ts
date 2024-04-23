28d6304a8e79d9db3512c9102dc775ceb3b20a95
Sheldon Debra <54963942+sheldondebra@users.noreply.github.com>
2024-04-23 21:24:49 +0000

    app

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
